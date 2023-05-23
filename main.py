23e169fb150d5eea83c42b8c308bd5b19d7d8fff
mateorobaina <mateorobainaa@gmail.com>
2023-05-22 22:12:48 -0300

    LAB 1 REALIZADO

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from nltk.book import text1, text7
+from nltk.tokenize import word_tokenize, re
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#mysent = "The cat sat on the mat."
+#mysent_tokens = word_tokenize(mysent)
+#print(len(mysent_tokens))
+#m =	re.search("leng",	"procesamiento	del	lenguaje natural")
+#print(m.start())
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+moby_dick_tokens = text1.tokens
+#1 Numeros de tokens en moby dick: 260819
+#print(len(moby_dick_tokens))
+#2 Numero de types: 19317
+types_mb = set(moby_dick_tokens)
+#print(len(types))
+#3 Type token ratio: 7.406285585022563
+#ttr_mobydick = (len(types_mb) / len(moby_dick_tokens)) * 100
+#print(ttr_mobydick)
+#4 Lo mismo para WSJ, su ratio es de 12.32468512853113
+wsj_tokens = text7.tokens
+#types_wsj = set(wsj_tokens)
+#ttr_wsj = (len(types_wsj) / len(wsj_tokens)) * 100
+#print(ttr_wsj)
+#5 WSJ
+#6 El diario abarca mas cantidad de topics a comparacion de moby dick.
+#7 el mle de la palabra whale es 0.003473673313677301 en moby dick.
+whale = "whale"
+ocurrencias_md = moby_dick_tokens.count(whale)
+mle_md = ocurrencias_md / len(moby_dick_tokens)
+#print(mle_md)
+ocurrencias_wsj = wsj_tokens.count(whale)
+mle_wsj = ocurrencias_wsj / len(wsj_tokens)
+print(mle_wsj)
+#7 el mle de la palabra whale es 0.0 en WSJ.
